82ea54c278e7bc0bb2ab775041846e780e47639e
Kim <kim.lang@di-unternehmer.com>
2022-12-07 00:58:36 +0100

    clean up

--- rollup.config.js
+++ rollup.config.js
@@ -11,9 +11,8 @@ import json from '@rollup/plugin-json';
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import babel from '@rollup/plugin-babel';
 import nodePolyfills from 'rollup-plugin-node-polyfills';
+import visualizer from 'rollup-plugin-visualizer';
 
-import React from 'react';
-import ReactDOM from 'react-dom';
 
 const packageJson = require("./package.json");
 
@@ -38,25 +37,22 @@ export default {
                 include: /node_modules/,
                 requireReturnsDefault: 'auto',
                 babelHelpers: "bundled",
-                // namedExports: {
-                //     'react': Object.keys(React),
-                //     'react-dom': Object.keys(ReactDOM),
-                // }
             }
         ),
         nodePolyfills(),
         nodeResolve({
-            browser: true,
-            // extensions: [".js"],
+            browser: true
         }),
-        json(),
         babel({
             babelHelpers: 'bundled',
             presets: ["@babel/preset-react"],
             babelrc: true,
             exclude: 'node_modules/**'
         }),
+        json(),
         terser(),
+        sizes(),
+        visualizer(),
 
         serve({
             open: false,
